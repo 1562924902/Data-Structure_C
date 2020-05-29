1925eccb22bd8664a209d77a1356541f07448782
1562924902@qq.com <1562924902@qq.com>
2020-05-29 09:22:10 +0800

    Third

--- HashTableDemo/HashTableAlgo.h
+++ HashTableDemo/HashTableAlgo.h
@@ -6,7 +6,7 @@ int InitHashTable(HashTable *H)
 	m=size;
 	H->count=m;
 	H->elem=(ElemType*)malloc(m*sizeof(ElemType));
-		for(i=0;i<m;i++){//初始化
+		for(i=0;i<m;i++){
 		H->elem[i].key = NULLKEY;
 		H->elem[i].otherinfo = -1;
 		}
@@ -15,22 +15,22 @@ int InitHashTable(HashTable *H)
  }
  
  
- 
-//Hash函数:使用除留余数法
+
  int Hash(int key)
- {//带除留余法
- 	return key%size;
+ {
+	key *=key;
+	return key/10000;
   } 
   
   
-//插入关键字到HashTable
+
   void InsertHash(HashTable *H,int key)
   {
-  	int addr=Hash(key);			//计算key对应地址
+  	int addr=Hash(key);			
   	while(H->elem[addr].key != NULLKEY)
   		addr=(addr+1)%size;
   	H->elem[addr].key = key;
-	H->elem[addr].otherinfo = addr;			//利用otherinfo存储key存储的位置
+	H->elem[addr].otherinfo = addr;			
    } 
   
   
--- HashTableDemo/HashTableUse.cpp
+++ HashTableDemo/HashTableUse.cpp
@@ -1,4 +1,4 @@
-#define size 12
+#define size 20		//哈希表长度为20
 #define NULLKEY -32768
 #include "HashTableDef.h"
 #include "pubuse.h"
@@ -7,20 +7,25 @@
 
 int main()
 {
-	int i;
+	int i,nun,value;
 	HashTable H;
-	int a[12]={12,65,34,16,27,37,22,28,15,47,48,35};
-	printf("----------测试代码----------");
-	printf("\n要插入的数字：");
-	for(i=0;i<size;i++)
-		printf("%d ",a[i]);
 	InitHashTable(&H);
-	for(i=0;i<size;i++)
+	printf("----------测试代码----------");
+	int a[size] ={309,312,321,318,305,317,301,304,328,331,409,416,432,410,411,415,413,422,431,420};
+	printf("\n要输入学生的学号为（三位数的学号）：");
+	for(i=0;i<size;i++){
+		printf("%d ",a[i]);
 		InsertHash(&H,a[i]);
+
+	}
+		
 	printf("\n插入之后的哈希表为：");
 	for(i=0;i<size;i++)
-		printf("(%d,%d)",H.elem[i].key,H.elem[i].otherinfo);
-	int addr,j;
-	j=SearchHash(H,a[6],&addr);
-	printf("\n搜索%d的地址是：%d\n",a[6],j);
+		printf("(%d,%d)  ",H.elem[i].key,H.elem[i].otherinfo);
+
+	int value1,addr,j;
+	printf("\n输入要查找的学号:");
+	scanf("%d",&value1);
+	j=SearchHash(H,value1,&addr);
+	printf("搜索的地址是：%d\n",j);
 }
